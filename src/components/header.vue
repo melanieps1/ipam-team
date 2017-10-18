15a45cfb69b8d92455bd5077aa820af6a579ff38
Clark Murray <clarkamurray@gmail.com>
2017-10-18 14:53:04 -0400

    Further styling

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
 <div id="app-header">
-  <v-toolbar id="blue-navbar" class="pt-3 pr-1 pl-2">
-    	<img id="ipam-logo-white" class="pb-2" src="../assets/ipamlogowhite.png">
+  <v-toolbar id="blue-navbar">
+    	<img id="ipam-logo-white" src="../assets/ipamlogowhite.png">
     	<v-spacer></v-spacer>
     	<span id="user">
     		Logged in as <span id="username">{{ user }}</span>
@@ -9,14 +9,17 @@
     		<button id="manageUsers">Log Out</button>
    	 	</span>
    </v-toolbar>
-   <v-tabs id="white-navbar">
-    <nav>
-	    <ul>
-	    	<li class="nav"><router-link to="/">SITES</router-link></li>
-	    	<li class="nav"><router-link to="/components">SUBNETS</router-link></li>
-	    	<li class="nav"><router-link to="/equipment">EQUIPMENT</router-link></li>
-	    </ul>
-    </nav>
+   <v-tabs v-model="active">
+        <v-tabs-bar id="white-navbar" class="white">
+          <v-tabs-item
+            v-for="tab in tabs"
+            :key = "tab"
+            :href="'#' + tab"
+          ><router-link v-bind:to="tab.path">
+            {{ tab.name }}
+          </router-link></v-tabs-item>
+          <v-tabs-slider color="blue"></v-tabs-slider>
+        </v-tabs-bar>
   	</v-tabs>
 </div>
 </template>
@@ -27,7 +30,23 @@
   	name: 'app-navbar',
   	data () {
   		return {
-  			user: "Melanie Stoeckle"
+  			user: "Melanie Stoeckle",
+  			tabs: [
+  				{
+  					name: 'sites',
+  					path: '/'
+
+  				},
+  				{
+  					name: 'subnets',
+  					path: '/subnets'
+  				},
+  				{
+  					name: 'equipment',
+  					path: '/equipment'
+  				}
+  			],
+  			active: null
   		}
   	}
   }
@@ -35,30 +54,28 @@
 
 <style>
 
-
 #blue-navbar {
 	background-color: #3F86BA;
 	height: 83px;
 }
 
+#ipam-logo-white {
+	margin-top: 24px;
+	margin-left: 40px;
+}
+
 
 #white-navbar {
-	background-color: #FFFFFF;
 	height: 55px;
 	width: 100%;
 	box-shadow: 0px 1px 6px 0 rgba(0,0,0,0.3);
+	font-weight: bold;
+	padding-left: 20px;
 }
 
-li.nav {
-	display: inline-block;
-	margin: 24px 37px 12px 0px;
-	font-weight: bold;
-	font-family: Roboto, sans-serif;
-	color: #757575;
-	font-size: 14px;
-	line-height: 19px;
-	width: 39px;
-	text-align: center;
+li.tabs__slider.blue {
+	height: 4px;
+	width: 80%;
 }
 
 a, a:visited {
@@ -68,6 +85,8 @@ a, a:visited {
 
 #user {
 	color: white;
+	margin-top: 25.5px;
+	margin-right: 39.5px;
 }
 
 #username {
@@ -78,8 +97,21 @@ a, a:visited {
 	box-sizing: border-box;
 	border: 1px white solid;
 	padding: 6.5px 12.5px;
-	margin-left: 48px;
+	margin-left: 23px;
 	border-radius: 2px;
+	line-height: 19px;
+	font-size: 14px;
+	height: 39.5px;
 }
+
+a.tabs__item.tabs__item--active {
+	font-size: 14px;
+	line-height: 19px;
+	padding-top: 24px;
+	padding-left: 20px;
+	padding-right: 20px;
+	display: inline-block;
+}
+
 
 </style>
--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import equipment from './components/equipment.vue'
 export default [
 
 {path: '/', component: sites},
-{path: '/components', component: subnets},
+{path: '/subnets', component: subnets},
 {path: '/equipment', component: equipment}
 
 ]
