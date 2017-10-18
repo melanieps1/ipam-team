79e51d6342b4513d551620ce2ecda1abd32e5ac2
Erin Brookins Wilcox <brookin8@gmail.com>
2017-10-18 13:38:30 -0400

    Working on filter function

--- src/components/equipment.vue
+++ src/components/equipment.vue
@@ -2,14 +2,49 @@
   <v-app light>
   	<div class="equipment">
     <v-container>
-        <v-text-field
-          append-icon="search"
-          label="Search"
-          single-line
-          hide-details
-          v-model="search"
-          class="mr-3">
-        </v-text-field>
+      
+      <v-container>
+        <v-layout row wrap>
+          <v-flex xs4>
+            <v-text-field
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+              v-model="search"
+              class="mr-3">
+            </v-text-field>
+          </v-flex>
+          <v-spacer></v-spacer>
+          <v-flex xs3>
+            <v-select
+              label="Select"
+              single-line
+              auto
+              props = "filter"
+              v-bind:headers = "select1headers"
+              v-bind:items = "selectSite.name"
+              hide-details>
+            </v-select>
+          </v-flex>
+          <v-spacer></v-spacer>
+          <v-flex xs3>
+            <v-select
+              label="Select"
+              single-line
+              auto
+              v-bind:items = "selectSubnet"
+              hide-details>
+            </v-select>
+          </v-flex>
+          <v-flex xs2>
+            <v-btn color="secondary" dark>
+              Add Equipment
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+      
       <v-card>
         <v-data-table
           v-bind:headers="headers"
@@ -45,11 +80,24 @@
 
   export default {
   	name: 'subnets',
-    mounted() {
+    beforeMount() {
       var self = this;
       axios.get("http://ipam-backend.herokuapp.com/api/equipment")
         .then(response => self.items = response.data);
+
+      axios.get("http://ipam-backend.herokuapp.com/api/sites")
+        .then(response => self.selectSite = response.data);
+      
+      for(var i=0;i<self.selectSite.length;i++) {
+          self.select1names.append(self.selectSite[i].name);
+        }
+
     },
+
+    mounted () {
+      console.log(this.items);
+    },
+
     data () {
       return {
         search: '',
@@ -73,7 +121,13 @@
         { text: 'Equipment Type', value: 'type[0].name' },
         { text: 'Notes', value: 'notes.text' }
         ],
-        items: []
+        select1headers: [
+          { text: 'Site', value: 'name' }
+        ],
+        items: [],
+        selectSite: [],
+        select1names: [],
+        selectSubnet: [],
       }
     }
   }
