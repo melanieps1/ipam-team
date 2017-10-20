21c9960491f0cf0b734cabe1b8713d7b5bce08f5
Clark Murray <clarkamurray@gmail.com>
2017-10-20 12:02:31 -0400

    Basic card form template

---
+++ src/components/form-card.vue
@@ -0,0 +1,59 @@
+<template>
+
+	<div id="formCard">
+		<v-card class="formCard" flat>
+			<v-subheader>
+				<i left class="material-icons">arrow_back</i>
+				<span>Back</span>
+				<h5 center>Add New Site</h5>
+			</v-subheader>
+			<v-container grid-list-md>
+				<v-layout row wrap>
+					<v-text-field box label="Site Name"></v-text-field>	
+				</v-layout>
+				<v-layout row wrap>
+					<v-text-field box label="Site Abbreviation"></v-text-field>
+				</v-layout>
+				<v-layout row wrap>
+					<v-text-field textarea label="Street Address"></v-text-field>
+				</v-layout>
+				<v-layout row wrap>
+					<v-text-field box textarea label="Site Contacts"></v-text-field>
+				</v-layout>
+				<v-layout row wrap>
+					<v-text-field box textarea label="Notes"></v-text-field>
+				</v-layout>
+			</v-container>
+		</v-card>
+			
+	</div>
+	
+</template>
+
+<style scoped>
+
+div.formCard.card {
+	width: 90%;
+	margin: 24px auto;
+	padding-bottom: 40px;
+}
+
+div.container.grid-list-md {
+	width: 100%;
+	margin: 0 auto;
+}
+
+h5 {
+	text-align: center;
+	font-weight: bold;
+	margin: 0 auto;
+}
+
+
+.formHeader {
+	margin: 0 auto;
+	padding-top: 24px;
+}
+
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import subnets from './components/subnets.vue'
 import equipment from './components/equipment.vue'
 import login from './components/login.vue'
 import forgotpassword from './components/forgot-password.vue'
+import formcard from './components/form-card.vue'
 
 export default [
 
@@ -10,6 +11,7 @@ export default [
 {path: '/subnets', component: subnets},
 {path: '/equipment', component: equipment},
 {path: '/login', component: login},
-{path: '/forgotpassword', component: forgotpassword}
+{path: '/forgotpassword', component: forgotpassword},
+{path: '/form-card', component: formcard}
 
 ]
